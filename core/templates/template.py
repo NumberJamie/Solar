554010c3bd9c5346df7baeafe8e09eb1b5676da1
NumberJamie <jamievanessen@gmail.com>
2024-03-10 13:26:03 +0100

    Update BaseTemplate

--- core/templates/template.py
+++ core/templates/template.py
@@ -6,13 +6,44 @@ from core.values import TEMPLATES
 
 
 class BaseTemplate:
+    template = 'error.html'
+
     def __init__(self, path: str, query: dict):
         self.env = Environment(
             loader=FileSystemLoader(TEMPLATES),
             autoescape=select_autoescape()
         )
-        self.path = path
         self.query = query
+        self.page = int(self.query.get('page', ['1'])[0])
+        self.path = [part for part in path.split('/') if part]
+        self.last = self.path[-1] if self.path else None
+        self._set_globals()
 
-    def error(self, status: HTTPStatus):
+    def render(self, **context) -> str:
+        self._set_globals()
+        return self.env.get_template(self.template).render(**context)
+
+    def get_bool(self, getter: str) -> bool | None:
+        if getter not in self.query or self.query[getter][0] not in ['0', '1']:
+            return None
+        return bool(int(self.query[getter][0]))
+
+    def get_str(self, getter: str) -> bool | None:
+        return self.query[getter][0] if getter in self.query else None
+
+    def get_list(self, getter: str) -> list:
+        return self.query[getter] if getter in self.query else []
+
+    def _set_globals(self) -> None:
+        self.env.globals.update(query=self._get_page())
+
+    def _get_page(self) -> str:
+        query_parts = [f'page={self.page + 1}']
+        for key, values in self.query.items():
+            if key == 'page':
+                continue
+            query_parts.extend([f'{key}={val}' for val in values])
+        return '?' + '&'.join(query_parts)
+
+    def error(self, status: HTTPStatus) -> str:
         return self.env.get_template('error.html').render(code=status.value, message=status.phrase)
